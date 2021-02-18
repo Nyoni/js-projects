de3a0f7fe16d48e78bebeb37b65d20c44cfee147
Nyoni <tafnyoni123@gmail.com>
2021-02-18 13:24:41 +0200

    final code

--- Change Background Color/changeColor.js
+++ Change Background Color/changeColor.js
@@ -1,1 +1,13 @@
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+const colors = ['green', 'yellow', 'black', 'blue', 'violet', 'red']
+body.style.backgroundColor = 'green'
 
+button.addEventListener('click', changeBackground)
+
+function changeBackground() {
+    const colorIndex= parseInt(
+        Math.random()*colors.length
+    )
+body.style.backgroundColor = colors[colorIndex]
+}
